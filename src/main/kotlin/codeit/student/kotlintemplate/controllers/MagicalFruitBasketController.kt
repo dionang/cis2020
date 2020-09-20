5d47fea603ba6c78744757346def91b84b1046c3
Dion Ang <dion_awx@hotmail.com>
2020-09-21 00:31:39 +0800

    Change endpoint to get for fruit basket

--- src/main/kotlin/codeit/student/kotlintemplate/controllers/MagicalFruitBasketController.kt
+++ src/main/kotlin/codeit/student/kotlintemplate/controllers/MagicalFruitBasketController.kt
@@ -1,7 +1,7 @@
 package codeit.student.kotlintemplate.controllers
 
 import org.slf4j.LoggerFactory
-import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController
 class MagicalFruitBasketController {
     private val logger = LoggerFactory.getLogger(this.javaClass)
 
-    @PostMapping("/fruitbasket ")
+    @GetMapping("/fruitbasket")
     fun getGuess(@RequestBody request: Map<String, Int>): Int {
         logger.info("Request received $request")
         val response = 0
