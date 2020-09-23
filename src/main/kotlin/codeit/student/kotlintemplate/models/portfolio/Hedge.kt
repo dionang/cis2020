76670008860989653a1dca2e7bd9344d0529f894
moses <moses_yong@yahoo.com.sg>
2020-09-23 10:50:04 +0800

    edited json format for optimize portfolio

--- src/main/kotlin/codeit/student/kotlintemplate/models/portfolio/Hedge.kt
+++ src/main/kotlin/codeit/student/kotlintemplate/models/portfolio/Hedge.kt
@@ -1,7 +1,10 @@
 package codeit.student.kotlintemplate.models.portfolio
 
 data class Hedge (
+    @JsonProperty("HedgePositionName")
     val HedgePositionName: String,
+    @JsonProperty("OptimalHedgingRatio")
     val OptimalHedgingRatio: Double,
+    @JsonProperty("NumFuturesContract")
     val NumFuturesContract: Int
 )
