12aefb4e9726021a69b9cdda53ca3e78bbee9145
moses <moses_yong@yahoo.com.sg>
2020-09-23 11:28:29 +0800

    edited json format for optimize portfolio annotation

--- src/main/kotlin/codeit/student/kotlintemplate/models/portfolio/Hedge.kt
+++ src/main/kotlin/codeit/student/kotlintemplate/models/portfolio/Hedge.kt
@@ -1,12 +1,12 @@
 package codeit.student.kotlintemplate.models.portfolio
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.databind.annotation.JsonNaming
 
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 data class Hedge (
-    @JsonProperty("HedgePositionName")
     val HedgePositionName: String,
-    @JsonProperty("OptimalHedgingRatio")
     val OptimalHedgingRatio: Double,
-    @JsonProperty("NumFuturesContract")
     val NumFuturesContract: Int
 )
