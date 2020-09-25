c0cb309f95e6e55f3291e69db1fe730b77fb80f4
Dion Ang <dion_awx@hotmail.com>
2020-09-25 21:30:26 +0800

    fast sort endpoint with fast method

--- src/main/kotlin/codeit/student/kotlintemplate/controllers/SortingController.kt
+++ src/main/kotlin/codeit/student/kotlintemplate/controllers/SortingController.kt
@@ -10,12 +10,21 @@ class SortingController {
     private val logger = LoggerFactory.getLogger(this.javaClass)
 
     @PostMapping("/sort")
-    fun sort(@RequestBody request: List<Int>):  List<Int> {
-//        logger.info("Request received $request")
-//        val response = BabylonResponse(
-//            evaluate(request.books, request.days)
-//        )
-//        logger.info("Returning result $response")
-        return request.sorted()
+    fun sort(@RequestBody numbers: List<Int>):  List<Int> {
+        return if (numbers.size < 10000) {
+            numbers.sorted()
+        } else {
+            val arr = MutableList(20001) { 0 }
+            for (num in numbers) {
+                arr[num+10000] += 1
+            }
+
+            val sorted = MutableList(numbers.size) { 0 }
+            var index = 0
+            for (i in 0..20000) {
+                repeat(arr[i]) { sorted[index++] = i-10000}
+            }
+            sorted
+        }
     }
 }
