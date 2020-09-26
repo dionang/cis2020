fec7b9fe9a90a49ece9fe347d01ea85de35e2d47
Dion Ang <dion_awx@hotmail.com>
2020-09-27 03:57:47 +0800

    Yin Yang test

--- src/main/kotlin/codeit/student/kotlintemplate/controllers/YinYangController.kt
+++ src/main/kotlin/codeit/student/kotlintemplate/controllers/YinYangController.kt
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import java.math.BigDecimal
 import kotlin.math.max
 
 @RestController
@@ -12,7 +13,7 @@ class YinYangController {
     private val logger = LoggerFactory.getLogger(this.javaClass)
 
     @PostMapping("/yin-yang")
-    fun evaluate(@RequestBody request: YinYangRequest): Map<String, Double> {
+    fun evaluate(@RequestBody request: YinYangRequest): Map<String, BigDecimal> {
         logger.info("Request received $request")
 
         val response = mapOf(
@@ -24,7 +25,7 @@ class YinYangController {
     }
 
     companion object {
-        fun calculateEV(elements: String, numberOfOperations: Int, memo: MutableMap<Pair<String, Int>, Double>): Double {
+        fun calculateEV(elements: String, numberOfOperations: Int, memo: MutableMap<Pair<String, Int>, BigDecimal>): BigDecimal {
             // save result in memo
             val combination = Pair(elements, numberOfOperations)
             if (memo.contains(combination)) {
@@ -33,20 +34,20 @@ class YinYangController {
 
             // base case: number of operations = 1
             if (numberOfOperations == 1) {
-                var prob = 0.0
+                var prob = BigDecimal.ZERO.setScale(10)
                 for (i in elements.indices) {
                     val left = elements[i]
                     val right = elements[elements.length - 1 - i]
 
-                    prob += if (left == 'y' && right == 'y') 0.0 else 1.0
+                    prob += if (left == 'y' && right == 'y') BigDecimal.ZERO else BigDecimal.ONE
                 }
 
-                val result = prob / elements.length
+                val result = prob / BigDecimal(elements.length)
                 memo[combination] = result
                 return result
             } else {
                 // recurse to children
-                var prob = 0.0
+                var prob = BigDecimal.ZERO
                 for (i in elements.indices) {
                     val left = elements[i]
                     val right = elements[elements.length - 1 - i]
@@ -56,19 +57,19 @@ class YinYangController {
 
                     if (left == right) {
                         // adds the result with the best ev (take letter from either left or right)
-                        prob += max(calculateEV(child1, numberOfOperations - 1, memo), calculateEV(child2, numberOfOperations - 1, memo))
+                        prob += calculateEV(child1, numberOfOperations - 1, memo).max(calculateEV(child2, numberOfOperations - 1, memo))
 
                         // adds ev of selecting Yang for current string
                         if (left == 'Y')
-                            prob += 1
+                            prob += BigDecimal.ONE
                     } else if (left == 'Y') {
-                        prob += 1 + calculateEV(child1, numberOfOperations - 1, memo)
+                        prob += BigDecimal.ONE + calculateEV(child1, numberOfOperations - 1, memo)
                     } else {
-                        prob += 1 + calculateEV(child2, numberOfOperations - 1, memo)
+                        prob += BigDecimal.ONE + calculateEV(child2, numberOfOperations - 1, memo)
                     }
                 }
 
-                val result = prob / elements.length
+                val result = prob / BigDecimal(elements.length)
                 memo[combination] = result
                 return result
             }
